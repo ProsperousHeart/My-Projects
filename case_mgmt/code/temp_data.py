df472c618ae18b619e8ec98762e28c7c00f4434a
Kassandra Keeton <ProsperousHeart@users.noreply.github.com>
2022-02-19 12:06:20 -0600

    additional data to begin playing with for testing/setup of code - no DB set up just yet

--- case_mgmt/code/temp_data.py
+++ case_mgmt/code/temp_data.py
@@ -1,3 +1,16 @@
 engr_ids = [{'id':"1234", 'f_name':"Rory", 'l_name':'Gilmore'},
             {'id':"5678", 'f_name':"Daniel", 'l_name':'Fizzlestick'}
             ]
+
+# temporary "database"
+case_db = [
+    {'case_no': 123, 'createdDate': "2022-02-01", 'pri': "3",
+     'status': "Open", 'lastModDate': "2022-02-18", 'engr_id': "1234"},
+    {'case_no': 124, 'createdDate': "2022-02-01", 'pri': "2",
+     'status': "Closed", 'lastModDate': "2022-02-18", 'engr_id': "1234"},
+    {'case_no': 125, 'createdDate': "2022-02-18", 'pri': "2",
+     'status': "Open", 'lastModDate': "2022-02-18", 'engr_id': "1234"}
+]
+
+case_status = ["Open", "Closed", "Escalated - BU", "Escalated - DM",
+               "On Hold", "Closed", "Reopened"]
