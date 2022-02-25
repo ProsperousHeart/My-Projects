63bce1715956dca89924efd5a09feecc99474b0a
Kassandra Keeton <ProsperousHeart@users.noreply.github.com>
2022-02-24 22:08:59 -0600

    slight clean up of positioning

--- case_mgmt/code/temp_data.py
+++ case_mgmt/code/temp_data.py
@@ -1,6 +1,7 @@
-engr_ids = [{'id':"1234", 'f_name':"Rory", 'l_name':'Gilmore'},
-            {'id':"5678", 'f_name':"Daniel", 'l_name':'Fizzlestick'}
-            ]
+engr_ids = [
+    {'id':"1234", 'f_name':"Rory", 'l_name':'Gilmore'},
+    {'id':"5678", 'f_name':"Daniel", 'l_name':'Fizzlestick'}
+]
 
 # temporary "database"
 case_db = [
